b2e23f46253fdf9bbadde5c6b0e1fc85e6807e7e
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-03-21 11:52:50 -0300

    Rubocop

--- lib/onemotors.rb
+++ lib/onemotors.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "onemotors/version"
+require_relative 'onemotors/version'
 require 'f1sales_custom/parser'
 require 'f1sales_custom/source'
 require 'f1sales_custom/hooks'
