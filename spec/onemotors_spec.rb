47a1c051a67dbd3d1cf36ced2bb97fcd0eac8adf
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-03-21 11:49:43 -0300

    First configurations

--- spec/onemotors_spec.rb
+++ spec/onemotors_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Onemotors do
   it "has a version number" do
     expect(Onemotors::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
