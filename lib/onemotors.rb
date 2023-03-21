bc31efd379e52915b7fea79cbb5c6702f32500b0
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-03-21 11:52:12 -0300

    Add gems

--- lib/onemotors.rb
+++ lib/onemotors.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "onemotors/version"
+require 'f1sales_custom/parser'
+require 'f1sales_custom/source'
+require 'f1sales_custom/hooks'
 
 module Onemotors
   class Error < StandardError; end
